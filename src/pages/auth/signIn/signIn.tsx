995483d83b14c744c195fec3227429cd483a6eb3
legendcatalan <legend.catalan@proton.me>
2024-08-07 18:01:58 +0600

    added SignUp and SignIn components

--- src/pages/auth/signIn/signIn.tsx
+++ src/pages/auth/signIn/signIn.tsx
@@ -2,23 +2,21 @@ import React from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { Link } from "react-router-dom";
-import { Button, Card } from "antd";
-import InputComponent from "../../common/components/Input/Input";
-import './SignIn.styles.scss';
-
-import useValidationSchemes from "../../common/validations/authScheme";
-import { Typography } from 'antd';
-import { useAuthStore } from "../../store/auth/auth.store";
-import { AuthData } from "../../store/auth/auth.type";
-import AlertInfo from "../../common/components/AlertInfo/AletInfo";
-import { Paths } from "../../routers/paths";
-import { useTranslation } from "../../common/locale/translation";
-const { Title, Text } = Typography;
+import { Button, Flex, Typography } from "antd";
+import { useAuthStore } from "@/store/auth/auth.store";
+import { Paths } from "@/routers/paths";
+import { useTranslation } from "@common/locale/translation";
+import useValidationSchemes from "@common/validations/authScheme";
+import InputComponent from "@common/components/Input/Input";
+import AlertInfo from "@common/components/AlertInfo/AletInfo";
+import AuthWrapper from "@/pages/auth/authWrapper";
+import { AuthData } from "@/store/auth/auth.type";
+const { Title,Text} = Typography;
 
 const SignIn = () => {
   const { signInError, signIn } = useAuthStore();
   const { auth } = useTranslation();
-  const { AuthScheme} = useValidationSchemes();
+  const { AuthScheme } = useValidationSchemes();
 
   const {
     control,
@@ -27,16 +25,16 @@ const SignIn = () => {
   } = useForm<AuthData>({
     resolver: yupResolver(AuthScheme)
   });
-
   const onLoginSubmit = async (data: AuthData) => {
     await signIn(data);
   };
 
   return (
-    <div className="auth">
-      <Card className='card'>
-        <Title className='title' level={2}>Вход</Title>
-        <form className="auth__form" onSubmit={handleSubmit(onLoginSubmit)}>
+    <AuthWrapper
+      title="Вход"
+    >
+      <form onSubmit={handleSubmit(onLoginSubmit)}>
+        <Flex justify='center' align='center' className="form">
           <InputComponent
             control={control}
             name="username"
@@ -54,11 +52,11 @@ const SignIn = () => {
             label="Пароль"
           />
           <AlertInfo message={signInError} type='error' />
-          <Text className='auth__link'>У вас еще нет учетной записи? <Link to={Paths.SIGN_UP} target="_blank">Зарегистрируйтесь сейчас</Link></Text>
+          <Text className='title'>У вас еще нет учетной записи? <Link to={Paths.SIGN_UP} target="_blank">Зарегистрируйтесь сейчас</Link></Text>
           <Button type="primary" htmlType="submit"><Title level={5}>{auth.signIn}</Title></Button>
-        </form>
-      </Card>
-    </div>
+        </Flex>
+      </form>
+    </AuthWrapper>
   );
 };
 
